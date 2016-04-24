6a8317e5414996114260363d1e689b7bdf427fa5
Eliott Davidson <eliott.davidson@gmail.com>
2016-04-24 16:54:17 -0700

    Added secret and like models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
 		self.email.downcase!
 	end
 
+	has_many :secrets
+	has_many :likes, dependent: :destroy
+	has_many :secrets_liked, through: :likes, source: :secret
 	has_secure_password
 	validates :name, presence: true
 	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
